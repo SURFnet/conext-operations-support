e27ee8730cbcce87f124e654e88e430c3e4af875
Alex Rothuis <arothuis@ibuildings.nl>
2015-10-02 10:56:25 +0200

    Create OperationsSupportBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Surfnet\JiraApiClientBundle\SurfnetJiraApiClientBundle(),
             new Surfnet\JanusApiClientBundle\SurfnetJanusApiClientBundle(),
+            new Surfnet\Conext\OperationsSupportBundle\SurfnetConextOperationsSupportBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
