2e9cac90213c884899a794e3a36734c788e5a9e6
Reinier Kip <reinier@ibuildings.nl>
2015-10-27 09:19:42 +0100

    Implement Guzzle transport for JIRA API client

--- src/Surfnet/JiraApiClientBundle/HttpClient/HttpClient.php
+++ src/Surfnet/JiraApiClientBundle/HttpClient/HttpClient.php
@@ -19,8 +19,11 @@
 namespace Surfnet\JiraApiClientBundle\HttpClient;
 
 use GuzzleHttp\ClientInterface as HttpClientInterface;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\GuzzleException;
 use Jira_Api_Authentication_AuthenticationInterface as AuthenticationInterface;
 use Jira_Api_Client_ClientInterface as JiraApiClientInterface;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
 final class HttpClient implements JiraApiClientInterface
@@ -49,7 +52,12 @@ final class HttpClient implements JiraApiClientInterface
     public function sendRequest($method, $url, $data = array(), $endpoint, AuthenticationInterface $credential)
     {
     // @codingStandardsIgnoreEnd
-        $options = [];
+        $options = [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+            'body' => json_encode($data),
+        ];
 
         if ($credential->getId() || $credential->getPassword()) {
             $options['auth'] = [
@@ -58,8 +66,57 @@ final class HttpClient implements JiraApiClientInterface
             ];
         }
 
-        $response = $this->client->request($method, $endpoint . $url, $options);
+        $this->logger->info(
+            sprintf('Sending %s request to %s', $method, $endpoint . $url),
+            ['payload' => $options['body']]
+        );
 
-        return $response->getBody();
+        try {
+            $response = $this->client->request($method, $endpoint . $url, $options);
+        } catch (ClientException $e) {
+            $response = $e->getResponse();
+
+            if ($response) {
+                $this->logger->error(
+                    sprintf('Received HTTP %d from JIRA API ("%s")', $response->getStatusCode(), $e->getMessage()),
+                    ['payload' => $this->readResponseBodyContents($response)]
+                );
+            } else {
+                $this->logger->error(
+                    sprintf('Unknown client error occurred while communicating with JIRA ("%s")', $e->getMessage())
+                );
+            }
+
+            throw $e;
+        } catch (GuzzleException $e) {
+            $this->logger->error(
+                sprintf('Guzzle exception occurred while communicating with JIRA ("%s")', $e->getMessage())
+            );
+
+            throw $e;
+        }
+
+        $responseContents = $this->readResponseBodyContents($response);
+        $this->logger->info('Received response from JIRA API', ['payload' => $responseContents]);
+
+        return $responseContents;
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @return null|string
+     */
+    private function readResponseBodyContents(ResponseInterface $response)
+    {
+        $body = $response->getBody();
+
+        if ($body->isSeekable()) {
+            $contents = $body->getContents();
+            $body->seek(0);
+        } else {
+            $contents = null;
+        }
+
+        return $contents;
     }
 }
